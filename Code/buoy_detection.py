9851075b0e50c7ee3cf903a896d9867781ee7dc8
umang <umang.rastogi96@gmail.com>
2020-04-05 01:31:53 -0400

    Implement buoy detection using various developed classes and methods

--- Code/buoy_detection.py
+++ Code/buoy_detection.py
@@ -1,34 +1,80 @@
 import cv2
 import numpy as np
+from sys import argv
+from utils import color_segmentation
 
-# TODO: Provide correct path to the project video
-# ../ means go to the outer folder
-# video = cv2.VideoCapture('../../videos/detectbuoy.avi')
-# while True:
-#     ret, video_frame = video.read()
-#     # If no video frame is generated or the video has ended
-#     if not ret:
-#         break
-#     cv2.imshow("Check", video_frame)
-#     cv2.waitKey(0)
-#
-# video.release()
+script, input_video_location, dataset_location, output_destination = argv
 
-# This image is extracted from the video. It is the first frame of the video
-# Apply blob detector on this image
-ref_img = cv2.imread('images/ref_img.png')
-# TODO: Define blob detector parameters to detect circles. Refer:
-#  https://www.learnopencv.com/blob-detection-using-opencv-python-c/
-params = cv2.SimpleBlobDetector_Params()
-params.filterByColor = True
-params.blobColor = 0
-params.filterByCircularity = True
-params.minCircularity = 0.8
-params.maxCircularity = 1
-detector = cv2.SimpleBlobDetector_create(params)
-# TODO: Define blob detector and get image with rich blobs
-key_points = detector.detect(ref_img)
-kp_img = cv2.drawKeypoints(ref_img, key_points, np.array([]), color=(0, 0, 0), flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-cv2.imshow("Blobs", kp_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    video = cv2.VideoCapture(str(input_video_location))  # Reading the video file
+    video_format = cv2.VideoWriter_fourcc('X', 'V', 'I', 'D')
+    video_output = cv2.VideoWriter(str(output_destination), video_format, 20.0, (640, 480))
+    # Train GMM models for various buoys
+    yellow_gmm = color_segmentation.gmm_train(str(dataset_location) + 'Yellow', 1, 50)
+    green_gmm = color_segmentation.gmm_train(str(dataset_location) + 'Green', 1, 50)
+    orange_gmm = color_segmentation.gmm_train(str(dataset_location) + 'Orange', 4, 200)
+    # Start iterating over each video frame
+    while True:
+        video_frame_exists, img = video.read()
+        if not video_frame_exists:
+            break
+        # print(img.shape)
+        # break
+        # Create copy of video frame to show buoy detection
+        img_result = img.copy()
+        # Fitting image
+        y_seg, y_img_shape, y_img_thresh, y_img_thresh_slack = color_segmentation.gmm_fit(img, None, yellow_gmm, 1,
+                                                                                          10e-3)
+        g_seg, g_img_shape, g_img_thresh, g_img_thresh_slack = color_segmentation.gmm_fit(img, None, green_gmm, 20, 10)
+        o_seg, o_img_shape, o_img_thresh, o_img_thresh_slack = color_segmentation.gmm_fit(img, 2, orange_gmm, 10e-15,
+                                                                                          10e-50)
+        seg = [y_seg, g_seg, o_seg]
+        img_shape = [y_img_shape, g_img_shape, o_img_shape]
+        img_thresh = [y_img_thresh, g_img_thresh, o_img_thresh]
+        img_thresh_slack = [y_img_thresh_slack, g_img_thresh_slack, o_img_thresh_slack]
+
+        #  Find bounding box
+        color = [(0, 255, 255), (0, 255, 0), (0, 0, 255)]
+        img_total = [img.copy(), img.copy(), img.copy()]
+        img_show = []
+        for i in range(len(seg)):
+            contours, _ = cv2.findContours(img_thresh[i], 1, 2)  # opencv3: return contours, cnts, hierarchy.
+            x = np.shape(img)[0] - 1
+            y = np.shape(img)[1] - 1
+            w = h = 0
+            if contours:
+                area = 100
+                for cnt in contours:
+                    if cv2.contourArea(cnt) > area:
+                        area = cv2.contourArea(cnt)
+                        x, y, w, h = cv2.boundingRect(cnt)
+                cv2.rectangle(seg[i], (x - int(0.5 * w), y - int(0.5 * h)), (x + int(1.5 * w), y + int(1.5 * h)),
+                              (0, 255, 0), 2)
+
+            mask = np.zeros((img_shape[i][0], img_shape[i][1])).astype(np.uint8)
+            cv2.rectangle(mask, (x - int(0.5 * w), y - int(1.5 * h)), (x + int(1.5 * w), y + int(1.5 * h)), 255, -1)
+            mask = cv2.bitwise_and(mask, img_thresh_slack[i].astype(np.uint8))
+            img_show.append(cv2.bitwise_and(img_total[i], img_total[i], mask=mask))
+            # Fit circle
+            contours, _ = cv2.findContours(mask, 1, 2)
+            if contours:
+                area = 0
+                center = (0, 0)
+                radius = 0
+                for cnt in contours:
+                    if cv2.contourArea(cnt) > area:
+                        area = cv2.contourArea(cnt)
+                        (x, y), radius = cv2.minEnclosingCircle(cnt)
+                        center = (int(x), int(y))
+                        radius = int(radius)
+                cv2.circle(img_result, center, radius, color[i], 2)
+
+        # cv2.imshow("Result", img_result)
+        # key = cv2.waitKey(1)
+        # if key == 27:
+        #     break
+        video_output.write(img_result)
+    # Destroy all open-cv windows
+    video.release()
+    video_output.release()
+    cv2.destroyAllWindows()
