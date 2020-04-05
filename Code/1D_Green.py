9ef7d55777d1ed08151fadba61b3f396f74afa5f
sroy0108 <sroy0108@umd.edu>
2020-04-04 21:19:28 -0400

    Remove unnecessary images from repo

--- Code/1D_Green.py
+++ Code/1D_Green.py
@@ -2,8 +2,9 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+from glob import glob
 
-video = cv2.VideoCapture('detectbuoy.avi')
+video = cv2.VideoCapture('/home/sayani_roy/Documents/underwater-buoy-detection/video/detectbuoy.avi')
 count = 0
 
 def mouse_click(event, x, y, flag, param):
@@ -27,46 +28,61 @@ def plot_histogram(img, title, mask=None):
         #avg = np.sum(hist, axis=1) / (hist.shape[1]-1)
         plt.plot(histogram,color = color)
         plt.xlim([0,256])
-    
+
+# count = 148
+# img_arr = []
+# for file_name in glob('images/Yellow/*.png'):
+#     img_arr.append(file_name)
+# img_arr.sort()
+# for file_name in img_arr:
+#     file = file_name.split('/')[2]
+#     try:
+#         if int(file[6:9]) > count:
+#             count += 1
+#             img = cv2.imread(file_name)
+#             cv2.imwrite('Yellow'+str(count)+'.png', img)
+#     except:
+#         print('Not integer')
+
 while video:
-    ret , video_frame = video.read()
+    ret, video_frame = video.read()
     if not ret:
         break
+    if count == 136:
+        # create folder in the directory where you want to store buoy dataset
+        directory = '/home/sayani_roy/Documents/underwater-buoy-detection/Code/images/Yellow'
+        pts = []
 
-    # create folder in the directory where you want to store buoy dataset    
-    directory = 'E:\Studies\Maryland\ENPM 673 Perception\Project 3\Green'
-    pts = []
-    
-    # resizing frame
-    re_frame = cv2.resize(video_frame, (1020,720), interpolation = cv2.INTER_AREA)
+        # resizing frame
+        re_frame = cv2.resize(video_frame, (1020,720), interpolation=cv2.INTER_AREA)
 
-    # Defining the mouse call back function for cropping
-    cv2.namedWindow("frame", 1)
-    cv2.setMouseCallback("frame", mouse_click)
-    cv2.imshow('frame', re_frame)
-    cv2.waitKey(0)
+        # Defining the mouse call back function for cropping
+        cv2.namedWindow("frame", 1)
+        cv2.setMouseCallback("frame", mouse_click)
+        cv2.imshow('frame', re_frame)
+        cv2.waitKey(0)
 
-    pts = np.array(pts, np.int32)
-    pts = pts.reshape((-1, 1, 2))
-    rectangle = cv2.boundingRect(pts)
-    x, y, w, h = rectangle
-    capture = re_frame[y:y + h, x:x + w].copy()
-  
-    # Directory for saving images
-    os.chdir(directory)
-    final_frame = cv2.polylines(re_frame, [pts], True, (0, 0, 0))
-    cv2.imshow('frame', final_frame)
-    
-    # Applying Masking
-    mask = np.zeros(capture.shape[:2], np.uint8)
-    points = pts - pts.min(axis=0)
-    cv2.drawContours(mask, [points], -1, (255, 255, 255), -1, cv2.LINE_AA)
-    masked = cv2.bitwise_and(capture, capture, mask=mask)
-    cv2.imshow("Masked image",masked)
-    cv2.imwrite('Green'+str(count)+'.png', masked)
-    cv2.waitKey(0)
+        pts = np.array(pts, np.int32)
+        pts = pts.reshape((-1, 1, 2))
+        rectangle = cv2.boundingRect(pts)
+        x, y, w, h = rectangle
+        capture = re_frame[y:y + h, x:x + w].copy()
+
+        # Directory for saving images
+        os.chdir(directory)
+        final_frame = cv2.polylines(re_frame, [pts], True, (0, 0, 0))
+        cv2.imshow('frame', final_frame)
+
+        # Applying Masking
+        mask = np.zeros(capture.shape[:2], np.uint8)
+        points = pts - pts.min(axis=0)
+        cv2.drawContours(mask, [points], -1, (255, 255, 255), -1, cv2.LINE_AA)
+        masked = cv2.bitwise_and(capture, capture, mask=mask)
+        cv2.imshow("Masked image",masked)
+        cv2.imwrite('Yellow'+str(count)+'.png', masked)
+        cv2.waitKey(0)
     count += 1
-    
+
 '''    
     images=[]
     img_save = "E:/Studies/Maryland/ENPM 673 Perception/Project 3/Green - Copy/"
